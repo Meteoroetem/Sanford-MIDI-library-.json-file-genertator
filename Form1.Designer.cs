416445a047575e6050d0ede5e56dda2213c2869a
Meteoroetem <114942884+Meteoroetem@users.noreply.github.com>
2022-12-05 20:57:07 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Sanford_MIDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pianoControl2 = new Sanford.Multimedia.Midi.UI.PianoControl();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pianoControl2
+            // 
+            this.pianoControl2.HighNoteID = 109;
+            this.pianoControl2.Location = new System.Drawing.Point(8, 116);
+            this.pianoControl2.LowNoteID = 21;
+            this.pianoControl2.Name = "pianoControl2";
+            this.pianoControl2.NoteOnColor = System.Drawing.Color.SkyBlue;
+            this.pianoControl2.Size = new System.Drawing.Size(788, 104);
+            this.pianoControl2.TabIndex = 0;
+            this.pianoControl2.Text = "pianoControl2";
+            this.pianoControl2.PianoKeyDown += new System.EventHandler<Sanford.Multimedia.Midi.UI.PianoKeyEventArgs>(this.pianoControl2_PianoKeyDown);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 233);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(801, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Maximum = 150;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(25, 17);
+            this.toolStripStatusLabel1.Text = "0, 0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.textBox1.Location = new System.Drawing.Point(284, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 38);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(8, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter .json file name:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(508, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 104);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(801, 255);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pianoControl2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "MIDI .json file generator";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Sanford.Multimedia.Midi.UI.PianoControl pianoControl1;
+        private Sanford.Multimedia.Midi.UI.PianoControl pianoControl2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sanford.Multimedia;
+using Sanford.Multimedia.Midi;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Sanford_MIDI
+{
+    public partial class Form1 : Form
+    {
+        InputDevice midiIn;
+        List<int> keysPressed= new List<int>();
+        List<int[]> notesPlayed = new List<int[]>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pianoControl2_PianoKeyDown(object sender, Sanford.Multimedia.Midi.UI.PianoKeyEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            midiIn = new InputDevice(0);
+            midiIn.StartRecording();
+            midiIn.ChannelMessageReceived += HandleChannelMessageRecieved;
+            
+        }
+
+        private void HandleChannelMessageRecieved(object sender, ChannelMessageEventArgs e)
+        {
+            int noteID = e.Message.Data1;
+            int timestamp = e.Message.Timestamp;
+            if (!keysPressed.Contains(noteID))
+            {
+                keysPressed.Add(noteID);
+                notesPlayed.Add(new int[2] {noteID,timestamp});
+                toolStripProgressBar1.Value = e.Message.Data2;
+            }
+            else
+            {
+                keysPressed.Remove(noteID);
+                pianoControl2.ReleasePianoKey(noteID);
+                //Sets the length of the note. 
+                notesPlayed.FindLast(note => note[0] == noteID)[1] = timestamp - notesPlayed.Find(note => note[0] == noteID)[1];
+                
+            }
+            foreach (int key in keysPressed)
+            {
+                pianoControl2.PressPianoKey(key);
+            }
+
+            
+            toolStripStatusLabel1.Text = $"{noteID}, {e.Message.Data2}";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            midiIn.StopRecording();
+            notesPlayed.Clear();
+            /*string path = @"C:\Users\rotem\Desktop\Programming\Sanford MIDI\Json output\output.json";
+
+            ArduinoSong song = new ArduinoSong(notesPlayed);
+
+            string jsonString = JsonConvert.SerializeObject(song);
+
+            using (FileStream fs = File.Create(path))
+            {
+                AddText(fs, jsonString);    
+            }*/
+        }
+        
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            midiIn.StopRecording();
+            string path;
+            if (textBox1.Text != null)
+            {
+                path = @"C:\Users\rotem\Desktop\Programming\Sanford MIDI\Json output\" + textBox1.Text + ".json";
+            }
+            else
+            {
+                path = @"C:\Users\rotem\Desktop\Programming\Sanford MIDI\Json output\output.json";
+            }
+            ArduinoSong song = new ArduinoSong(notesPlayed);
+
+            string jsonString = JsonConvert.SerializeObject(song);
+
+            using (FileStream fs = File.Create(path))
+            {
+                AddText(fs, jsonString);
+            }
+            notesPlayed.Clear();
+            //midiIn.StartRecording();
+        }
+    }
+
+    class ArduinoSong
+    {
+        public List<int> notes = new List<int>();
+        public List<int> lengths = new List<int>();
+
+        public ArduinoSong(List<int[]> playedNotes)
+        {
+            List<int> notes = new List<int>();
+            List<int> lengths = new List<int>();
+            foreach (int[] notePlayed in playedNotes)
+            {
+                notes.Add(notePlayed[0]);
+                lengths.Add(notePlayed[1]);
+            }
+            this.notes = notes;
+            this.lengths = lengths;
+        }
+    }
+}
